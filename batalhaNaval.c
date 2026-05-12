d0db358e8535d1684bd8ad2a3d2e4361ec50b0f9
nicksestacio <202602550563@alunos.estacio.br>
2026-05-12 18:29:04 -0300

    Update batalhaNaval Nível Mestre.c

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,45 +1,52 @@
 #include <stdio.h>
 
 int main() {
-    int tabuleiro[10][10] = {0};
+    int cone[5][5] = {
+        {0, 0, 1, 0, 0},
+        {0, 1, 1, 1, 0},
+        {1, 1, 1, 1, 1},
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0}
+    };
+
+    int octaedro[5][5] = {
+        {0, 0, 1, 0, 0},
+        {0, 1, 1, 1, 0},
+        {1, 1, 1, 1, 1},
+        {0, 1, 1, 1, 0},
+        {0, 0, 1, 0, 0}
+    };
+
+    int cruz[5][5] = {
+        {0, 0, 1, 0, 0},
+        {0, 0, 1, 0, 0},
+        {1, 1, 1, 1, 1},
+        {0, 0, 1, 0, 0},
+        {0, 0, 1, 0, 0}
+    };
+
     int i, j;
 
-    int navioVertical[3][2] = {
-        {1, 1},
-        {2, 1},
-        {3, 1}
-    };
-
-    int navioHorizontal[3][2] = {
-        {5, 3},
-        {5, 4},
-        {5, 5}
-    };
-
-    int navioDiagonal1[3][2] = {
-        {0, 0},
-        {1, 1},
-        {2, 2}
-    };
-
-    int navioDiagonal2[3][2] = {
-        {7, 2},
-        {8, 3},
-        {9, 4}
-    };
-
-    for (i = 0; i < 3; i++) {
-        tabuleiro[navioVertical[i][0]][navioVertical[i][1]] = 3;
-        tabuleiro[navioHorizontal[i][0]][navioHorizontal[i][1]] = 3;
-        tabuleiro[navioDiagonal1[i][0]][navioDiagonal1[i][1]] = 3;
-        tabuleiro[navioDiagonal2[i][0]][navioDiagonal2[i][1]] = 3;
+    printf("Habilidade em cone:\n");
+    for (i = 0; i < 5; i++) {
+        for (j = 0; j < 5; j++) {
+            printf("%d ", cone[i][j]);
+        }
+        printf("\n");
     }
 
-    printf("Tabuleiro Batalha Naval:\n\n");
+    printf("\nHabilidade em octaedro:\n");
+    for (i = 0; i < 5; i++) {
+        for (j = 0; j < 5; j++) {
+            printf("%d ", octaedro[i][j]);
+        }
+        printf("\n");
+    }
 
-    for (i = 0; i < 10; i++) {
-        for (j = 0; j < 10; j++) {
-            printf("%d ", tabuleiro[i][j]);
+    printf("\nHabilidade em cruz:\n");
+    for (i = 0; i < 5; i++) {
+        for (j = 0; j < 5; j++) {
+            printf("%d ", cruz[i][j]);
         }
         printf("\n");
     }
